b9249539588468e8ac0cee917b8c420a45010d45
Amirabbas Ghasemi <amirabbas08dev@gmail.com>
2025-10-23 15:01:07 +0330

    feat: add move name step

---
+++ codemods/v9/scripts/move.sh
@@ -0,0 +1,12 @@
+set -e
+
+find . -type f \( -name '.eslintrc.js' -o -name '.eslintrc.json' -o -name '.eslintrc.yaml' -o -name '.eslintrc.yml' \) | while read file; do
+  dir=$(dirname "$file")
+  new_path="${dir}/eslint.config.cjs"
+  if [[ -e "${new_path}" ]]; then
+    echo "❌ ${new_path} already exists. Skipping moving ${file}."
+    continue
+  fi
+  mv "$file" "$new_path"
+  echo "✅ Moved $file -> $new_path"
+done
