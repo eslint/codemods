1c16f5a6ec80276a0539704f0d4d884660800c21
Amirabbas Ghasemi <amirabbas08dev@gmail.com>
2025-12-11 06:09:56 +0330

    fix: match test cases with actual results and fix shell step

--- codemods/v9/config-file/scripts/move.sh
+++ codemods/v9/config-file/scripts/move.sh
@@ -1,10 +1,9 @@
 find . -type f \( -name '.eslintrc.js' -o -name '.eslintrc.json' -o -name '.eslintrc.yaml' -o -name '.eslintrc.yml' \) | while read file; do
-  dir=$(dirname "$file")
-  new_path="${dir}/eslint.config.mjs"
+  new_path="$(dirname "${file}")/eslint.config.mjs"
   if [[ -e "${new_path}" ]]; then
     echo "❌ ${new_path} already exists. Skipping moving ${file}."
     continue
   fi
-  mv "$file" "$new_path"
-  echo "✅ Moved $file -> $new_path"
+  mv "${file}" "${new_path}"
+  echo "✅ Moved ${file} -> ${new_path}"
 done
---
+++ codemods/v9/config-file/tests/fixtures/json/after/eslint.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        myCustomGlobal: readonly,
+        jQuery: readonly,
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaVersion: "latest"
+      },
+      sourceType: "module"
+    },
+    rules: {
+      "no-console": ["warn", { "allow": ["warn", "error"] }],
+      "no-sequences": ["error", {"allowInParentheses": false}],
+      "no-unused-vars": ["error", {"caughtErrors":"all","vars":"all","args":"after-used"}],
+      "no-useless-computed-key": ["error", {enforceForClassMembers: true}],
+      "camelcase": ["error", {"properties":"always","ignoreDestructuring":false}]
+    },
+  },
+  {
+    files: ["*.test.js", "*.spec.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      },
+      parserOptions: {}
+    },
+    rules: {
+      "no-console": "off"
+    },
+  }
+]);
